fd0b9cce64fac242c6f5663a3103453b2de51ff8
Isabel Lee <ijl0322@gmail.com>
2015-12-09 14:18:46 +0800

    bouncing ball implemented

--- pingpong.py
+++ pingpong.py
@@ -1,4 +1,4 @@
-import pygame, sys, time
+import pygame, sys, time, random
 from pygame.locals import *
 
 pygame.init()
@@ -7,8 +7,8 @@ window = pygame.display.set_mode((400, 600), 0, 32)
 pygame.display.set_caption("Ping Pong")
 
 SPEED = 4
-DIR_X = 1
-DIR_Y = 1
+DIR_X = 2
+DIR_Y = 2
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -20,7 +20,7 @@ TEAL = (0,255,255)
 
 
 
-ball = pygame.Rect(100, 150, 60, 60)
+ball = pygame.Rect(100, 150, 40, 40)
 #pygame.draw.circle(window, BLACK, (200, 500), 20, 0)
 
 
@@ -29,22 +29,24 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    window.fill(WHITE)
             
     for i in range(10):
         for j in range(5):
             color = [RED, GREEN, BLUE, TEAL, PINK]
             pygame.draw.rect(window, color[j], ((i*40) + 2 , (j*25) + 5, 36, 20))
             
-    window.fill(WHITE)
+
             
     ball.left += SPEED*DIR_X
     ball.top += SPEED*DIR_Y
 
-    if ball.left < 0 or ball.left > 400:
+    if ball.left < 0 or ball.left > 360:
         DIR_X *= -1
-    if ball.top < 0 or ball.top > 600:
+    if ball.top < 0 or ball.top > 560:
         DIR_Y *= -1
     
-    pygame.draw.rect(window, BLACK, ball)        
+    pygame.draw.circle(window, BLACK, ball.center, 20)   
     pygame.display.update()
     time.sleep(0.02)
